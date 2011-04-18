a150360ac11a2b602a0277094e6a349278cf3d41
Jack Edge <yellowbounder@gmail.com>
2011-04-18 15:03:24 +0100

    fdSIFSDAJDFSAJFDS

--- banli.py
+++ banli.py
@@ -5,23 +5,26 @@ from twisted.words.protocols import irc
 from twisted.internet import protocol
 
 class BanliBot(irc.IRCClient):
+    nickname = "banli"
     def connectionMade(self):
         irc.IRCClient.connectionMade(self)
     def signedOn(self):
-        self.setNick("banli")
         self.join(self.factory.channel)
     def userJoined(self,user,channel):
         if channel == self.factory.channel:
             self.say(self.factory.channel,"OMG HAI {0}".format(user.upper()))
     def privmsg(self,user,channel,msg):
-        print user,channel,msg
-        if channel == self.nickname:
-            last_bothered = self.factory.private_messagers.get(user,0)
-            diff = time.time() - last_bothered
-            if diff > 600:
-                msg = "Private messages, srsly?"
-                self.msg(user,msg)
-                self.factory.private_messagers[user] = time.time()
+        if channel == self.nickname and user.split('!')[0] == "coiax":
+            #last_bothered = self.factory.private_messagers.get(user,0)
+            #diff = time.time() - last_bothered
+            #if diff > 600:
+                #    msg = "Private messages, srsly?"
+                ###self.msg(user,msg)
+                #self.factory.private_messagers[user] = time.time()
+            msg = msg.lower()
+            self.say(self.factory.channel,msg)
+
+
         if channel == self.factory.channel:
             if msg.find(self.nickname) != -1:
                 msg = "{0}: tsup".format(user.split('!')[0])
@@ -31,7 +34,7 @@ class BanliFactory(protocol.ClientFactory):
 
     protocol = BanliBot
 
-    def __init__(self,channel="#awesome"):
+    def __init__(self,channel="#que"):
         self.channel = channel
         self.private_messagers = {}
 
